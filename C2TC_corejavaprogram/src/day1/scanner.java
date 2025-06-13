e9c51d91931804d319ec4991e97e5f5f245536c9
Jabaraj28 <sjabaraj427@gmail.com>
2025-06-13 09:25:04 +0530

    This my java Scanner for addition program.

---
+++ C2TC_corejavaprogram/src/day1/scanner.java
@@ -0,0 +1,17 @@
+package day1;
+import java.util.Scanner;
+public class scanner {
+	public static void main(String[]args){
+	int a,b,c;
+	Scanner ac;
+	ac= new Scanner(System.in);
+	System.out.println("enter a ");
+	a = ac.nextInt();
+	System.out.println("enter b");
+	b = ac.nextInt();
+	c = a+b;
+	System.out.println("addtion of a+b="+c);
+}
+}
+	
+	
