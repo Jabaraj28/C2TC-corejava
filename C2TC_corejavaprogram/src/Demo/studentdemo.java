dbc5d8f93784f7d3e24e28e0075d2e6c93555e4a
Jabaraj28 <sjabaraj427@gmail.com>
2025-06-13 12:24:50 +0530

    This is my Getter and Setter Java program.

---
+++ C2TC_corejavaprogram/src/Demo/studentdemo.java
@@ -0,0 +1,21 @@
+package Demo;
+
+public class studentdemo {
+	public static void main(String[]arge) {
+			student st = new student();
+			st.setId(101);
+			st.setName("Jaba");
+			st.setAge(21);
+			
+			System.out.println(st);
+			
+			student st1 = new student();
+			st1.setId(101);
+			st1.setName("Jaba");
+			st1.setAge(21); 
+			
+			System.out.println(st1);
+			
+			
+	}
+}
