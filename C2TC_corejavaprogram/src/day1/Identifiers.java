68b9b686b2026a820bd6b0719b54f976d837d6b0
Jabaraj28 <sjabaraj427@gmail.com>
2025-06-13 10:34:57 +0530

    This my Identifier java program.

---
+++ C2TC_corejavaprogram/src/day1/Identifiers.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class Identifiers {
+	public static void main(String[]args) {
+		
+		int number = 2;
+		System.out.println("value of the number is:"+number);
+		
+	    String studentname="Jaba";
+	    System.out.println("value is:"+studentname);
+	
+	}
+
+}
